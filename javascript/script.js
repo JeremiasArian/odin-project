ea284f0962707ff47144593d96677d06ae4d5796
JeremiasArian <159722626+JeremiasArian@users.noreply.github.com>
2024-06-24 22:33:59 -0300

    Variables and Operators

---
+++ javascript/script.js
@@ -0,0 +1,18 @@
+let admin;
+let name;
+
+name = "John";
+admin = name;
+
+console.log()
+console.log("This and the next lines are inside a Javascript file linked to the HTML file:")
+console.log("Value of admin: " + admin + " (it´s admin)");
+console.log("Value of name: " + name)
+
+console.log()
+
+let planetName = "Earth";
+let currentUser = "user";
+
+console.log(planetName + ". " + currentUser)
+
